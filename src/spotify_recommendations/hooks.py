7a0758d16ae448cb4e3828e3e33e2afd504ecb3b
Anish Shah <anish.shah.emails@gmail.com>
2021-08-21 03:14:08 -0400

    Add scraping and experimentation notebooks

--- src/spotify_recommendations/hooks.py
+++ src/spotify_recommendations/hooks.py
@@ -27,9 +27,10 @@
 # limitations under the License.
 
 """Project hooks."""
+import os
 from typing import Any, Dict, Iterable, Optional
 
-from kedro.config import ConfigLoader
+from kedro.config import ConfigLoader, TemplatedConfigLoader
 from kedro.framework.hooks import hook_impl
 from kedro.io import DataCatalog
 from kedro.versioning import Journal
@@ -39,8 +40,10 @@ class ProjectHooks:
     @hook_impl
     def register_config_loader(
         self, conf_paths: Iterable[str], env: str, extra_params: Dict[str, Any],
-    ) -> ConfigLoader:
-        return ConfigLoader(conf_paths)
+    ) -> TemplatedConfigLoader:
+        return TemplatedConfigLoader(conf_paths, globals_dict={
+            "DATA_DIR": os.environ.get("DATA_DIR", "data")
+        })
 
     @hook_impl
     def register_catalog(
